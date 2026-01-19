f4298def1efea5981e1dde52eb726c126ade255f
ens20mpnm <124590968+ens20mpnm@users.noreply.github.com>
2026-01-19 14:34:19 +0100

    uppdaterat rätt

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App(){
         <Route path="/projects" element={<Projects />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/ProjektDagbok" element={<Contact />} />
+        <Route path="/ProjektDagbok" element={<ProjektDagbok />} />
       </Routes>
     </div>
   )
